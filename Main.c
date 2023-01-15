ed893265e1e1d5ca80f7a70eac323ce94073f88c
Simmond <Simmond@DESKTOP-761LV3S>
2023-01-15 09:16:11 +0300

    Add project files.

---
+++ Main.c
@@ -0,0 +1,8 @@
+// A first program in c
+#include <stdio.h>
+
+//The mai function begans heere
+int main(void) {
+	printf("Writing program in C is very exciting. I am so proud to have started this journey");
+}
+//end of the main function
