b57a641f426996fe4fc899f89a661dbd07e50e10
simongakii123 <simowagura9@gmail.com>
2023-01-16 15:01:21 +0300

    Improved the program to perform some basic calculations.

--- Main.c
+++ Main.c
@@ -3,8 +3,32 @@
 
 //The mai function begans heere
 int main(void) {
-	printf("Writing program in C is very exciting. I am so proud to have started this journey");
+	int integer1, integer2; // This variables will hold the input entered by the user
+	
+	printf("Enter first integer\n"); //prompt
+	scanf("%d", &integer1);// read an integer
+
+	printf("Enter the second integer\n"); // Prompt for the second integer
+	scanf("%d", &integer2);
+
+	int sum = integer1 + integer2; // perform the  additional calculation
+	int sub = integer1 - integer2; //perform substraction
+    int division = integer1 / integer2; // perform the division calculation
+
+	printf("The integers you provided are %d,%d\n", integer1,  integer2);
+	printf("Sum is %d\n", sum);
+	printf("Substraction is %d\n", sub);
+	printf("Division is %d\n", division);
+
+
+
+
+
+
+
 }
 //end of the main function
-//guys
-//I am here
+// end of the intoduction part
+
+//
+
